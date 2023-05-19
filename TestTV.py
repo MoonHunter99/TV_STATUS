cd4f149ea2d7d866d42b2651904b59334b1d6d73
Neil Daniel Vergara <vergaraneildaniel@gmail.com>
2023-05-19 20:30:26 +0800

    Added decorations ready for recording

--- TestTV.py
+++ TestTV.py
@@ -4,5 +4,7 @@ from TV import Tv
 TV1 = Tv("30" , "3" , "")
 TV2 = Tv("3" , "2" , "")
 #calling the methods
-TV1.TV(), TV1.GetChannel(), TV1.GetVolume()
-TV2.TV(), TV2.GetChannel(), TV2.GetVolume()
+print("\033[1;39m `"* 112)
+print("\nThis is a TV Status Checker\n")
+TV1.TV(), TV1.GetChannel(), TV1.GetVolume(), print("\033[1;39m `"* 112)
+TV2.TV(), TV2.GetChannel(), TV2.GetVolume() , print("\033[1;39m `"* 112)
